91f19e47211a4b07d04ec03c70e2ce2e98f689fc
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-11-08 15:38:30 +0100

    Creation des carrés de base du plateau

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import Square from './components/square';
+import React, { Component } from 'react';
+import Square from './components/Square'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="title">
+          <h1> Les Petits chevaux </h1>
+          <span><button>Restart</button></span>
+
+
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Square className="top-left" />
+              <Square className="top-right" />
+            </div>
+
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <Square className="bottom-left" />
+              <Square className="bottom-right" />
+            </div>
+
+          </div>
+
+
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Square extends Component {
     render() {
         return (
             <div>
-                hello
-                
+                <div className={this.props.className}></div>
             </div>
-        )
+        );
     }
 }
 
-export default Square
+export default Square;
