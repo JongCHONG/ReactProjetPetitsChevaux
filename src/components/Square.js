5361e4ee25476101ec4890612d09ef45c9e4fc7a
chaimakhorchid <khorchid.chaima@hotmail.fr>
2021-11-09 10:14:55 +0100

    modif

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { Component } from 'react'
+import Circle from './Circle';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <div
-        style={{
-          display: "grid",
-          justifyContent: "space-between",
-          alignContent: "center,",
-          gridTemplateRows: "2",
-          gridTemplateColumns: "2",
-          gridRow: `${this.props.row}`,
-          gridColumn: `${this.props.column}`,
-          width: "300px",
-          height: "300px",
-          border: `3px solid black`,
-          padding: "40px",
-          backgroundColor: `${this.props.color}`,
-        }}
-      >
-        {this.props.children}
-      </div>
-    );
-  }
+class Square extends Component {
+    render() {
+        return (
+            <>
+                <div className="col-5">
+                    {this.props.className === "top-left" &&
+                        <div className="big-square d-flex">
+                            <div className="top-left"></div>
+                            <Circle />
+                         </div>
+                    }
+                    {this.props.className === "top-right" && 
+                        <div className="big-square d-flex justify-content-end ">
+                            <div className="top-right"></div>
+                        </div>
+                    }
+                    {this.props.className === "bottom-left" &&
+                        <div className="big-square d-flex align-items-end">
+                            <div className="bottom-left"></div>
+                        </div>
+                    }
+                    {this.props.className === "bottom-right" && 
+                        <div className="big-square d-flex justify-content-end align-items-end">
+                            <div className="bottom-right"></div>
+                        </div>
+                    }
+                </div>
+            </>
+        );
+    }
 }
+
 export default Square;
