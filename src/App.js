3215af456e7c30679284a5022bee59a196a77690
Edouard-Germain <edangermain@gmail.com>
2021-11-09 12:08:24 +0100

    fonction mouvement

--- src/App.js
+++ src/App.js
@@ -6,7 +6,49 @@ import './App.css'
 
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      counter: 0,
+      player: [
+        { 
+          id : 0,
+          name : "Player 1",
+          color : "blue",
+          positionInitiale : 0,
+          positionActuelle : 0,
+          positionFinale: 56
+        }
+      ]
+      // ,
+      //  player2: {
+      //   id : 1,
+      //   name : "Player 2",
+      //   color : "red",
+      //   positionInitiale : 0,
+      //   positionActuelle : 0
+      // }
+    }
+    this.handlePlayerMove= this.handlePlayerMove.bind(this)
+
+  }
+  handlePlayerMove=()=>{           //e=dice//
+    const { id, name, color,  positionInitiale, positionActuelle, positionFinale } = this.state.player[0]
+    let newPosition = positionActuelle+6
+    const newArray = {
+      id: id,
+      name: name,
+      color: color,
+      positionInitiale: positionInitiale,
+      positionActuelle: newPosition,
+      positionFinale : positionFinale  
+    }
+    this.setState({player:[ newArray]})
+  }
+
   render() {
+
+    console.log(this.state.player)
     return (
       <>
         <div className="container">
@@ -14,6 +56,7 @@ class App extends Component {
             <div className="head d-flex align-items-center justify-content-between">
               <h1>Les Petits Chevaux</h1>
               <button>Restart</button>
+              <button onClick={this.handlePlayerMove}>test</button>
             </div>
           </div>
           <div className="row">
